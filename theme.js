edacfb09b68416834b071c09f4e84e382fd0ca4e
AigarsReinholds <92621012+AigarsReinholds@users.noreply.github.com>
2021-10-16 09:25:46 +0300

    Add files via upload
    Failu izvietošana

---
+++ theme.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("navigationMobile");
+};
+
+function closeNav() {
+    document.getElementById("navigationMobile");
+};
+
+function scrollDown() {
+if (location.hash.length !== 0) {
+window.scrollTo(0,document.body.scrollHeight);
+    }
+};
+
