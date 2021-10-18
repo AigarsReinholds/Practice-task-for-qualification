b83e27d06e7a59e2b46cda0be1966e8fc0f16384
AigarsReinholds <92621012+AigarsReinholds@users.noreply.github.com>
2021-10-18 19:22:09 +0300

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,14 +1,15 @@
 function openNav() {
-    document.getElementById("navigationMobile");
-};
+    document.getElementById("navigationMobile").style.width = "100%";
+}
 
 function closeNav() {
-    document.getElementById("navigationMobile");
-};
+    document.getElementById("navigationMobile").style.width = "0";
+}
 
 function scrollDown() {
 if (location.hash.length !== 0) {
 window.scrollTo(0,document.body.scrollHeight);
     }
 };
+
 
